de5f9ed99a0df69b248233d0387431de842e4f6e
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-03-23 18:49:20 +0100

    shadcn configuration

--- app/root.tsx
+++ app/root.tsx
@@ -6,9 +6,14 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import styles from "./globals.css";
+import styles from "./globals.css?url";
+import { LinksFunction } from "@remix-run/node";
+import { cssBundleHref } from "@remix-run/css-bundle";
 
-
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,6 +12,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <Button>Click me!</Button>
       <ul>
         <li>
           <a
