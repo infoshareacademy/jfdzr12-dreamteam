1c08f5c28b4bf55c92f60d9f42a3249e48dc76d0
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-03-24 14:34:50 +0100

    Added useTheme & onClick in button Sun/Moon

--- app/blocks/header.tsx
+++ app/blocks/header.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { Link } from "@remix-run/react"
 import { Button } from "~/atoms/ui/button"
 import { Container } from "~/atoms/ui/container"
 import { Sun, Moon } from "lucide-react"
+import { ProfileButton } from "~/blocks/profileButton"
+import { useTheme } from "next-themes";
 
 
 /*routes roboczo */
@@ -30,6 +34,7 @@ const routes = [
 ]
 
 export const Header = () => {
+  const { theme, setTheme } = useTheme();
   return (
     <header className="sm:flex sm:justify-between py-3 px-4 border-b">
       <Container>
@@ -53,11 +58,13 @@ export const Header = () => {
             ))}
           </nav>
           <div className="flex items-center">
-            <Button variant={"ghost"} size={"icon"} className="mr-6" aria-label="Toggle Theme">
+            <Button variant={"ghost"} size={"icon"} className="mr-6" aria-label="Toggle Theme"
+              onClick={() => { setTheme(theme === 'dark' ? 'light' : 'dark') }}>
               <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
               <Moon className="absoluteh-6 w-6 rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
               <span className="sr-only">Toggle Theme</span>
             </Button>
+            <ProfileButton />
           </div>
         </div>
       </Container>
