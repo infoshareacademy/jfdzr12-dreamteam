c203f9aa2d820ffe0e1c7a445a32ae271bdfd687
Monika Pławska <monikaplawska88@gmail.com>
2024-03-24 16:34:43 +0100

    EventForm rendered in _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Button } from "~/atoms/ui/button";
 import { Card } from "~/atoms/ui/card";
+import { EventForm } from "~/blocks/eventForm";
 import { GuestsForm } from "~/blocks/guestsForm";
 
 export const meta: MetaFunction = () => {
@@ -14,8 +15,9 @@ export default function Index() {
   return (
     <div className="grid place-items-center h-screen" /*style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}*/>
       <Card>  
-         <GuestsForm onSubmit={(e)=>{console.log("Onsubmit", e)}}/>
-    </Card>
+        <GuestsForm onSubmit={(e)=>{console.log("Onsubmit", e)}}/>
+      </Card>
+      <EventForm/>
     </div>
   );
 }
