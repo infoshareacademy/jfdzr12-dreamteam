dbce232cda0eb032a4180afdac9ac3ea2d5b6da4
Monika Proczek <monikaproczek9@gmail.com>
2024-03-27 17:07:50 +0100

    interface better than type

--- app/blocks/formForGuest.tsx
+++ app/blocks/formForGuest.tsx
@@ -1,96 +1,3 @@
-// import React, { useState } from 'react';
-// import { Button } from "~/atoms/ui/button";
-// import { Label } from "~/atoms/ui/label";
-
-// interface NameFormProps {
-//   onSubmit: (isPresenceChecked: string, isPartnerChecked: string | undefined) => void;
-// }
-
-// export const FormForGuest: React.FC<NameFormProps> = ({ onSubmit }) => {
-//   const [isPresenceChecked, setIsPresenceChecked] = useState<string | undefined>();
-//   const [showAdditionalQuestions, setShowAdditionalQuestions] = useState<boolean>(false);
-//   const [isPartnerChecked, setIsPartnerChecked] = useState<string | undefined>();
-
-//   const handleRadioPresenceChange = (value: string) => {
-//     setIsPresenceChecked(value);
-//     if (value === "Potwierdzam przybycie") {
-//       setShowAdditionalQuestions(true);
-//     } else {
-//       setShowAdditionalQuestions(false);
-//     }
-//   };
-
-//   const handleRadioPartnerChange = (value: string) => {
-//     setIsPartnerChecked(value);
-//   };
-
-//   const handleSubmit = (e: React.FormEvent) => {
-//     e.preventDefault();
-//     if (isPresenceChecked !== undefined && (!showAdditionalQuestions || isPartnerChecked !== undefined)) {
-//       // console.log(isPresenceChecked, isPartnerChecked)
-//       onSubmit(isPresenceChecked, isPartnerChecked);
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit} id="IdFormForGuest" method="post" action="/guest">
-//       <Label htmlFor="presence">Potwierdzenie przybycia:</Label>
-//       <div>
-//         <input 
-//           name="presence"
-//           type="radio" 
-//           id="presence-yes" 
-//           value={isPresenceChecked} 
-//           onChange={() => handleRadioPresenceChange("Potwierdzam przybycie")}
-//           required
-//         />
-//         <Label htmlFor="presence-yes">Tak</Label>
-//       </div>
-//       <div>
-//         <input
-//           name="presence" 
-//           type="radio" 
-//           id="presence-no" 
-//           value={isPresenceChecked} 
-//           onChange={() => handleRadioPresenceChange("Odmawiam przybycia")} 
-//           required
-//         />
-//         <Label htmlFor="presence-no">Nie</Label>
-//       </div>
-//       {/* {showAdditionalQuestions && (
-//         <> */}
-//           <Label htmlFor="partner">Obecność Osoby Towarzyszącej:</Label>
-//           <div>
-//           <input 
-//               name="partner" 
-//               type="radio" 
-//               id="partner-yes" 
-//               value={isPartnerChecked} 
-//               onChange={() => handleRadioPartnerChange("z Osobą Towarzyszącą")} 
-//               required
-//             />
-//             <Label htmlFor="partner-yes">Tak</Label>
-//           </div>
-//           <div>
-//           <input 
-//               name="partner" 
-//               type="radio" 
-//               id="partner-no" 
-//               value={isPartnerChecked} 
-//               onChange={() => handleRadioPartnerChange("bez Osoby Towarzyszącej")} 
-//               required
-//             />
-//             <Label htmlFor="partner-no">Nie</Label>
-//           </div>
-//         {/* </>
-//       )} */}
-
-//       <Button type="submit">Wyślij</Button>
-//     </form>
-//   );
-// }
-
-
 import React, { useState } from 'react';
 import { Button } from "~/atoms/ui/button";
 import { Label } from "~/atoms/ui/label";
@@ -101,13 +8,13 @@ const textLabelPresence = "Potwierdzenie przybycia : ";
 const textLabelPartner = "Obecność Osoby Towarzyszącej : ";
 const textButtonSubmit = "Wyślij";
 
-// Type reprezentujący obiekt przekazywany do onSubmit
-type FormValues = {
-  isPresenceChecked: string | undefined;
-  isPartnerChecked: string | undefined;
+// interface reprezentujący obiekt przekazywany do onSubmit
+interface FormValues {
+  isPresenceChecked: string;
+  isPartnerChecked?: string;
 }
 
-type NameFormProps = {
+interface NameFormProps {
   onSubmit: (objectValues: FormValues) => void;
 }
 
