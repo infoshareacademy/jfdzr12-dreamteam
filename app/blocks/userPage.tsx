d5f3bf39837e462a5ac6563d2568d3033a80f74f
Monika Pławska <monikaplawska88@gmail.com>
2024-03-29 12:39:52 +0100

    createCodeManager, getRandomCode and generateUniqueCode functions created in userPage

--- app/blocks/userPage.tsx
+++ app/blocks/userPage.tsx
@@ -1,7 +1,50 @@
-const UserPage = (): React.ReactElement => {
+interface CodeManager {
+    codes: number[];
+    min: number;
+    max: number;
+}
+
+function createCodeManager(min: number, max: number): CodeManager {
+    return {
+        codes: [],
+        min,
+        max,
+    }
+}
+
+function getRandomCode(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateUniqueCode(manager: CodeManager): number {
+    let code: number;
+    
+    while(true) {
+        code = getRandomCode(manager.min, manager.max);
+        if(!manager.codes.includes(code)) {
+            break;
+        }
+    }
+
+    manager.codes.push(code);
+
+    if(manager.codes.length === manager.max - manager.min + 1) {
+        manager.codes = [];
+    }
+
+    console.log(manager.codes);
+
+    return code;
+}
+
+const codeManager = createCodeManager(1000, 9000);
+const eventCode = generateUniqueCode(codeManager);
+
+
+export const UserPage = (): React.ReactElement => {
     return (
         <div>
-            <p></p>
+            <p>{eventCode}</p>
             <p></p>
         </div>
     )
