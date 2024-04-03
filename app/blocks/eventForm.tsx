928c2091814efdd1298d8751243c0b9013ad9c0e
Monika Pławska <monikaplawska88@gmail.com>
2024-04-03 16:19:24 +0200

    EventForm validation added

--- app/blocks/eventForm.tsx
+++ app/blocks/eventForm.tsx
@@ -13,6 +13,46 @@ import { Input } from '~/atoms/ui/input';
 import { Label } from '~/atoms/ui/label';
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '~/atoms/ui/card';
 import { DatePicker } from './datePicker';
+import errorsToRecord from '@hookform/resolvers/io-ts/dist/errorsToRecord.js';
+import { Alert } from '~/atoms/ui/alert';
+import { validateInputsPhoneNumbers, validateInputTimeFormat, validateInputsStringValues } from '~/lib/utils';
+
+// const formField = {
+//     Names: {
+//         brideNameLabel: "Bride's",
+//         brideNameInput: "brideName",
+//         groomNameLabel: "Groom's",
+//         groomNameInput: "groomName",
+//     },
+//     Event: {
+//         eventDateLabel: "Date",
+//         eventDateInput: "eventDate",
+//         eventTimeLabel: "Time",
+//         eventTimeInput: "eventTime",
+//     },
+//     Ceremony: {
+//         ceremonyPlaceLabel: "Place",
+//         ceremonyPlaceInput: "ceremonyPlace",
+//         ceremonyAddressLabel: "Address",
+//         ceremonyAddressInput: "ceremonyAddress",
+//     },
+//     Reception: {
+//         receptionPlaceLabel: "Place",
+//         receptionPlaceInput: "receptionPlace",
+//         receptionAddressLabel: "Address",
+//         receptionAddressInput: "receptionAddress",
+//     },
+//     PhoneNumbers: {  // Zmieniono na "PhoneNumbers"
+//         brideNumberLabel: "Bride's",
+//         brideNumberInput: "brideNumber",
+//         groomNumberLabel: "Groom's",
+//         groomNumberInput: "groomNumber",
+//     },
+//     Other: {
+//         leadColorLabel: "Lead color",
+//         leadColorInput: "leadColor",
+//     }
+// }
 
 interface EventFormData {
     brideName: string;
@@ -29,7 +69,7 @@ interface EventFormData {
 }
 
 interface InputsErrors {
-    [key: string]: string | Date;
+    [key: string]: string;
 }
 
 export const EventForm = (): React.ReactElement => {
@@ -48,17 +88,71 @@ export const EventForm = (): React.ReactElement => {
         leadColor: '',
     })
 
-    const [errors, setErrors] = useState<InputsErrors>({});
+    const [inputsErrors, setInputsErrors] = useState<InputsErrors>({});
 
     const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
     
+        const newErrors: InputsErrors = {};
+    
+        if (!formData.brideName.trim()) {
+            newErrors.brideName = "Enter bride's name";
+        } else if (!validateInputsStringValues(formData.brideName)) {
+            newErrors.brideName = 'Use at least two letters'
+        }
+        if (!formData.groomName.trim()) {
+            newErrors.groomName = "Enter groom's name";
+        } else if (!validateInputsStringValues(formData.groomName)) {
+            newErrors.groomName = 'Use at least two letters'
+        }
         if (!formData.eventDate) {
-            alert('Please select a date.');
+            newErrors.eventDate = 'Select date';
+        }
+        if (!formData.eventTime.trim()) {
+            newErrors.eventTime = 'Enter event time';
+        } else if (!validateInputTimeFormat(formData.eventTime)) {
+            newErrors.eventTime = 'Time format is 00:00'
+        }
+        if (!formData.ceremonyPlace.trim()) {
+            newErrors.ceremonyPlace = 'Enter ceremony place';
+        } else if (!validateInputsStringValues(formData.ceremonyPlace)) {
+            newErrors.ceremonyPlace = 'Use at least two letters'
+        }
+        if (!formData.ceremonyAddress.trim()) {
+            newErrors.ceremonyAddress = 'Enter ceremony address';
+        }
+        if (!formData.receptionPlace.trim()) {
+            newErrors.receptionPlace = 'Enter reception place';
+        } else if (!validateInputsStringValues(formData.receptionPlace)) {
+            newErrors.receptionPlace = 'Use at least two letters'
+        }
+        if (!formData.receptionAddress.trim()) {
+            newErrors.receptionAddress = 'Enter reception address';
+        }
+        if (!formData.brideNumber.trim()) {
+            newErrors.brideNumber = "Enter bride's number";
+        } else if (!validateInputsPhoneNumbers(formData.brideNumber)) {
+            newErrors.brideNumber = 'Use at least 6 digits.'
+        }
+        if (!formData.groomNumber.trim()) {
+            newErrors.groomNumber = "Enter groom's number";
+        } else if (!validateInputsPhoneNumbers(formData.groomNumber)) {
+            newErrors.groomNumber = 'Use at least 6 digits.'
+        }
+        if (!formData.leadColor.trim()) {
+            newErrors.leadColor = 'Select lead color of your event';
+        }
+    
+        if (Object.keys(newErrors).length > 0) {
+            setInputsErrors(newErrors);
+            console.log('Errors:', inputsErrors);
             return;
         }
     
-
+        console.log('Form submitted successfully')
     };
+
+    console.log(formData)
 
     // obliczenie lczby dni pozostałych do wydarzenia
     const today = new Date();
@@ -74,7 +168,7 @@ export const EventForm = (): React.ReactElement => {
                 <CardTitle className='text-center'>Your dream event</CardTitle>
             </CardHeader>
             <CardContent>
-                <form>
+                <form id='EventForm' /*onSubmit={handleSubmit}*/>
                     <fieldset>
                         
                         <div className='w-full'>
@@ -89,6 +183,7 @@ export const EventForm = (): React.ReactElement => {
                                             onChange={(e) => setFormData(data => ({...data, brideName: e.target.value}))}
                                             required
                                         />
+                                        {inputsErrors.brideName && <div className="text-red-500">{inputsErrors.brideName}</div>}
                                     </div>
                                     <div className='flex flex-col space-y-1.5'>
                                         <Label>Groom's</Label>
@@ -98,6 +193,7 @@ export const EventForm = (): React.ReactElement => {
                                             onChange={(e) => setFormData(data => ({...data, groomName: e.target.value}))}
                                             required
                                         />
+                                        {inputsErrors.groomName && <div className="text-red-500">{inputsErrors.groomName}</div>}
                                     </div>
                                 </div>
                             </legend>
@@ -110,6 +206,7 @@ export const EventForm = (): React.ReactElement => {
                                             value={formData.eventDate} 
                                             onSelectDate={(date) => setFormData (data => ({...data, eventDate: date}))}
                                         />
+                                        {inputsErrors.eventDate && <div className="text-red-500">{inputsErrors.eventDate}</div>}
                                     </div>
                                     <div className='flex flex-col space-y-1.5 mb-5'>
                                         <Label>Time</Label>
@@ -119,6 +216,7 @@ export const EventForm = (): React.ReactElement => {
                                             onChange={(e) => setFormData(data => ({...data, eventTime: e.target.value}))}
                                             required
                                         />
+                                        {inputsErrors.eventTime && <div className="text-red-500">{inputsErrors.eventTime}</div>}
                                     </div>
                                 </div>
                             </legend>
@@ -133,6 +231,7 @@ export const EventForm = (): React.ReactElement => {
                                             onChange={(e) => setFormData(data => ({...data, ceremonyPlace: e.target.value}))}
                                             required
                                         />
+                                        {inputsErrors.ceremonyPlace && <div className="text-red-500">{inputsErrors.ceremonyPlace}</div>}
                                     </div>
                                     <div className='flex flex-col space-y-1.5'>
                                         <Label>Address</Label>
@@ -143,6 +242,7 @@ export const EventForm = (): React.ReactElement => {
                                             onChange={(e) => setFormData(data => ({...data, ceremonyAddress: e.target.value}))}
                                             required
                                         />
+                                        {inputsErrors.ceremonyAddress && <div className="text-red-500">{inputsErrors.ceremonyAddress}</div>}
                                     </div>
                                 </div>
                             </legend>
@@ -157,6 +257,7 @@ export const EventForm = (): React.ReactElement => {
                                             onChange={(e) => setFormData(data => ({...data, receptionPlace: e.target.value}))}
                                             required
                                         />
+                                        {inputsErrors.receptionPlace && <div className="text-red-500">{inputsErrors.receptionPlace}</div>}
                                     </div>
                                     <div className='flex flex-col space-y-1.5 mb-5'>    
                                         <Label>Address</Label>
@@ -167,6 +268,7 @@ export const EventForm = (): React.ReactElement => {
                                             onChange={(e) => setFormData(data => ({...data, receptionAddress: e.target.value}))}
                                             required
                                         /> 
+                                        {inputsErrors.receptionAddress && <div className="text-red-500">{inputsErrors.receptionAddress}</div>}
                                     </div>
                                 </div>
                             </legend>
@@ -182,6 +284,7 @@ export const EventForm = (): React.ReactElement => {
                                             onChange={(e) => setFormData(data => ({...data, brideNumber: e.target.value}))}
                                             required
                                         />
+                                        {inputsErrors.brideNumber && <div className="text-red-500">{inputsErrors.brideNumber}</div>}
                                     </div>
                                     <div className='flex flex-col space-y-1.5 mb-5'>
                                         <Label>Groom's</Label>
@@ -192,6 +295,7 @@ export const EventForm = (): React.ReactElement => {
                                             onChange={(e) => setFormData(data => ({...data, groomNumber: e.target.value}))}
                                             required
                                         />
+                                        {inputsErrors.groomNumber && <div className="text-red-500">{inputsErrors.groomNumber}</div>}
                                     </div>
                                 </div>
                             </legend>
@@ -207,6 +311,7 @@ export const EventForm = (): React.ReactElement => {
                                             onChange={(e) => setFormData(data => ({...data, leadColor: e.target.value}))}
                                             required
                                         />
+                                        {inputsErrors.leadColor && <div className="text-red-500">{inputsErrors.leadColor}</div>}
                                     </div>
                                 </div>
                             </legend>
@@ -214,10 +319,11 @@ export const EventForm = (): React.ReactElement => {
                         </div>
                     </fieldset>
                 </form>
+
             </CardContent>
             <CardFooter className='grid grid-cols-2 gap-4'>
                 <Button className='w-full'>Cancel</Button>
-                <Button type='button' className='w-full'>Add your event</Button>
+                <Button type='submit' form='EventForm' className='w-full' onClick={handleSubmit}>Add your event</Button>
             </CardFooter>
         </Card>
     );
