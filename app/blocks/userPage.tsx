b76eae6df54e99cc0362c3f9805b05b3e297085e
Monika Pławska <monikaplawska88@gmail.com>
2024-04-04 14:14:45 +0200

    generateUniqueCode and UniqueCodeGenerator replaced from userPage to utils

--- app/blocks/userPage.tsx
+++ app/blocks/userPage.tsx
@@ -1,63 +1,7 @@
-interface CodeManager {
-    codes: number[];
-    min: number;
-    max: number;
-}
-
-function createCodeManager(min: number, max: number): CodeManager {
-    return {
-        codes: [],
-        min,
-        max,
-    }
-}
-
-function getRandomCode(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function generateUniqueCode(manager: CodeManager): number {
-    let code: number;
-    
-    while(true) {
-        code = getRandomCode(manager.min, manager.max);
-        if(!manager.codes.includes(code)) {
-            break;
-        }
-    }
-
-    manager.codes.push(code);
-
-    if(manager.codes.length === manager.max - manager.min + 1) {
-        manager.codes = [];
-    }
-
-    console.log(manager.codes);
-
-    return code;
-}
-
-const codeManager = createCodeManager(1000, 9000);
-const eventCode = generateUniqueCode(codeManager);
-
-function* UniqueCodeGenerator() {
-    let code = 1000;
-
-    while (true) {
-        yield code++;
-    }
-}
-
-const uniqueCodeGenerator = UniqueCodeGenerator();
-console.log(uniqueCodeGenerator.next().value);
-console.log(uniqueCodeGenerator.next().value);
-console.log(uniqueCodeGenerator.next().value);
-
-
 export const UserPage = (): React.ReactElement => {
     return (
         <div>
-            <p>{eventCode}</p>
+            <p></p>
             <p></p>
         </div>
     )
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -24,3 +24,59 @@ export function validateInputsPhoneNumbers(value: string): boolean {
   const digitsOnly = value.replace(/\D/g, '');
   return digitsOnly.length >= 6;
 }
+
+
+// interface CodeManager {
+//   codes: number[];
+//   min: number;
+//   max: number;
+// }
+
+// function createCodeManager(min: number, max: number): CodeManager {
+//   return {
+//       codes: [],
+//       min,
+//       max,
+//   }
+// }
+
+// function getRandomCode(min: number, max: number): number {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// function generateUniqueCode(manager: CodeManager): number {
+//   let code: number;
+  
+//   while(true) {
+//       code = getRandomCode(manager.min, manager.max);
+//       if(!manager.codes.includes(code)) {
+//           break;
+//       }
+//   }
+
+//   manager.codes.push(code);
+
+//   if(manager.codes.length === manager.max - manager.min + 1) {
+//       manager.codes = [];
+//   }
+
+//   console.log(manager.codes);
+
+//   return code;
+// }
+
+// const codeManager = createCodeManager(1000, 9000);
+// const eventCode = generateUniqueCode(codeManager);
+
+export function* UniqueCodeGenerator() {
+  let code = 1000;
+
+  while (true) {
+      yield code++;
+  }
+}
+
+const uniqueCodeGenerator = UniqueCodeGenerator();
+console.log(uniqueCodeGenerator.next().value);
+console.log(uniqueCodeGenerator.next().value);
+console.log(uniqueCodeGenerator.next().value);
