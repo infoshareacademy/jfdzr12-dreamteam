09a0546be51b62fb4f036b2b87210601d5640b02
Aleksandra Boroń <alek166194@wgmail.com>
2024-04-06 21:10:50 +0200

    Changes to be committed: new file:   app/blocks/signUp.tsx modified:   app/routes/_index.tsx new file:   app/routes/sign-in.tsx new file:   app/routes/sign-up.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -30,6 +30,5 @@ export default function Index() {
 
     </div>
       
-    </div>
   );
 }
---
+++ app/routes/sign-up.tsx
@@ -0,0 +1,7 @@
+import { SignUp } from "~/blocks/signUp"
+export default function SignUpPage (){
+return (
+<SignUp/>
+
+)}
+
