98c3a3e3c89c3f0a679e3d7ee8b449eec893c4df
Monika Pławska <monikaplawska88@gmail.com>
2024-04-15 23:00:39 +0200

    validation modified, console.logs deleted, border-b replaced form CardFooter to div

--- app/routes/new-event.tsx
+++ app/routes/new-event.tsx
@@ -5,7 +5,6 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "~/atoms/ui
 import { DatePicker } from "~/atoms/ui/date-picker";
 import { Input } from "~/atoms/ui/input";
 import { Label } from "~/atoms/ui/label";
-import { Textarea } from "~/atoms/ui/textarea";
 import { eventRef } from "~/db/event-ref";
 import { uniqueCodeGenerator } from "~/lib/utils";
 
@@ -27,7 +26,7 @@ interface NewEventFormData {
 
 type FormErrorData<T> = Partial<Record<keyof T, string>>
 
-export default function NewEventPage () {
+export default function NewEventPage() {
 
     const [error, setError] = useState<FormErrorData<NewEventFormData>>();
     const [eventDate, setEventDate] = useState<Date | undefined>();
@@ -37,62 +36,64 @@ export default function NewEventPage () {
             return
         }
         event.preventDefault();
-        const _formData = new FormData(event.target)
+        const _formData = new FormData(event.target);
         console.log("_formData", _formData)
         if(eventDate) {
             _formData.append("eventDate", eventDate.toString());
-        }
+        };
 
         const nextID = await uniqueCodeGenerator.next();
-        console.log("nextID", nextID.value);
-        _formData.append("eventID", String(nextID.value))
+        _formData.append("eventID", String(nextID.value));
 
-        const formData = Object.fromEntries(_formData.entries())
-        console.log("submit", formData, event)
+        const formData = Object.fromEntries(_formData.entries());
 
         const errors: FormErrorData<NewEventFormData> = {};
 
         if(!("firstPerson" in formData && typeof formData.firstPerson === "string" && formData.firstPerson.length > 2)) {
             errors.firstPerson = "Use at least 2 characters"
-        }
+        };
         if(!("secondPerson" in formData && typeof formData.secondPerson === "string" && formData.secondPerson.length > 2)) {
             errors.secondPerson = "Use at least 2 characters"
-        }
+        };
         if(!("eventDate" in formData)) {
             errors.eventDate = "Choose event date"
-        }
+        };
         if(!("eventTime" in formData && typeof formData.eventTime === "string" && formData.eventTime.length > 2)) {
             errors.eventTime = "Enter event time"
-        }
+        };
         if(!("ceremonyPlace" in formData && typeof formData.ceremonyPlace === "string" && formData.ceremonyPlace.length > 2)) {
             errors.ceremonyPlace = "Enter ceremony place, use at least 2 characters"
-        }
+        };
         if(!("ceremonyStreetAddress" in formData && typeof formData.ceremonyStreetAddress === "string" && formData.ceremonyStreetAddress.length > 2)) {
             errors.ceremonyStreetAddress = "Enter ceremony street address, use at least 2 characters"
-        }
+        };
         if(!("ceremonyCityAddress" in formData && typeof formData.ceremonyCityAddress === "string" && formData.ceremonyCityAddress.length > 2)) {
             errors.ceremonyCityAddress = "Enter ceremony city address, use at least 2 characters"
-        }
+        };
         if(!("receptionPlace" in formData && typeof formData.receptionPlace === "string" && formData.receptionPlace.length > 2)) {
             errors.receptionPlace = "Enter reception place, use at least 2 characters"
-        }
+        };
         if(!("receptionStreetAddress" in formData && typeof formData.receptionStreetAddress === "string" && formData.receptionStreetAddress.length > 2)) {
             errors.receptionStreetAddress = "Enter reception street address, use at least 2 characters"
-        }
+        };
         if(!("receptionCityAddress" in formData && typeof formData.receptionCityAddress === "string" && formData.receptionCityAddress.length > 2)) {
             errors.receptionCityAddress = "Enter reception city address, use at least 2 characters"
-        }
+        };
         if(!("firstPersonPhone" in formData && typeof formData.firstPersonPhone === "string" && formData.firstPersonPhone.length >= 6)) {
             errors.firstPersonPhone = "Enter first person's number. Use at least 6 numbers"
-        }
+        };
         if(!("secondPersonPhone" in formData && typeof formData.secondPersonPhone === "string" && formData.secondPersonPhone.length >= 6)) {
             errors.secondPersonPhone = "Enter second person's number. Use at least 6 numbers"
-        }
+        };
 
-        setError(errors)
-        
-        addDoc(eventRef, formData)
-    
+        setError(errors);
+
+        if(Object.keys(errors).length !== 0) {
+            return;
+        } else {
+            addDoc(eventRef, formData);
+            event.target.reset();
+        }
     }
 
     return (
@@ -102,7 +103,7 @@ export default function NewEventPage () {
             </CardHeader>
             <CardContent>
                 <form id="EventForm" onSubmit={handleOnSubmit}>
-                    <div className="w-full">
+                    <div className="w-full border-b-2 pb-4">
 
                         <div className="text-lg font-bold mb-2"><p className="border-t-2 py-4 mb-4">Names</p>
                             <div className="grid grid-cols-3 gap-4">
@@ -202,7 +203,7 @@ export default function NewEventPage () {
                     </div>
                 </form>
             </CardContent>
-            <CardFooter className="grid grid-cols-3 gap-4 border-t-2 py-8">
+            <CardFooter className="grid grid-cols-3 gap-4">
                 <Button className="col-start-2" variant="outline">Cancel</Button>
                 <Button type="submit" form="EventForm" >Add your event</Button>
             </CardFooter>
