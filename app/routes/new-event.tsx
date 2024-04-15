7d3aada48003b60c6cafa1209a84d8a4df07fdd4
Monika Pławska <monikaplawska88@gmail.com>
2024-04-15 22:05:17 +0200

    inputs ceremonyStreetAddress, ceremonyCityAddress, receptionStreetAddress, receptionCityAddress added, grid-cols-3

--- app/routes/new-event.tsx
+++ app/routes/new-event.tsx
@@ -16,9 +16,11 @@ interface NewEventFormData {
     eventDate: string,
     eventTime: string,
     ceremonyPlace: string,
-    ceremonyAddress: string,
+    ceremonyStreetAddress: string,
+    ceremonyCityAddress: string,
     receptionPlace: string,
-    receptionAddress: string,
+    receptionStreetAddress: string,
+    receptionCityAddress: string,
     firstPersonPhone: string,
     secondPersonPhone: string,
 }
@@ -38,12 +40,12 @@ export default function NewEventPage () {
         const _formData = new FormData(event.target)
         console.log("_formData", _formData)
         if(eventDate) {
-            _formData.append('eventDate', eventDate.toString());
+            _formData.append("eventDate", eventDate.toString());
         }
 
         const nextID = await uniqueCodeGenerator.next();
-        console.log('nextID', nextID.value);
-        _formData.append('eventID', String(nextID.value))
+        console.log("nextID", nextID.value);
+        _formData.append("eventID", String(nextID.value))
 
         const formData = Object.fromEntries(_formData.entries())
         console.log("submit", formData, event)
@@ -51,28 +53,34 @@ export default function NewEventPage () {
         const errors: FormErrorData<NewEventFormData> = {};
 
         if(!("firstPerson" in formData && typeof formData.firstPerson === "string" && formData.firstPerson.length > 2)) {
-            errors.firstPerson = 'Use at least 2 characters'
+            errors.firstPerson = "Use at least 2 characters"
         }
         if(!("secondPerson" in formData && typeof formData.secondPerson === "string" && formData.secondPerson.length > 2)) {
-            errors.secondPerson = 'Use at least 2 characters'
+            errors.secondPerson = "Use at least 2 characters"
         }
         if(!("eventDate" in formData)) {
-            errors.eventDate = 'Enter event date'
+            errors.eventDate = "Choose event date"
         }
         if(!("eventTime" in formData && typeof formData.eventTime === "string" && formData.eventTime.length > 2)) {
-            errors.eventTime = 'Enter event time'
+            errors.eventTime = "Enter event time"
         }
         if(!("ceremonyPlace" in formData && typeof formData.ceremonyPlace === "string" && formData.ceremonyPlace.length > 2)) {
-            errors.ceremonyPlace = 'Enter ceremony place, use at least 2 characters'
+            errors.ceremonyPlace = "Enter ceremony place, use at least 2 characters"
         }
-        if(!("ceremonyAddress" in formData && typeof formData.ceremonyAddress === "string" && formData.ceremonyAddress.length > 2)) {
-            errors.ceremonyAddress = 'Enter ceremony address, use at least 2 characters'
+        if(!("ceremonyStreetAddress" in formData && typeof formData.ceremonyStreetAddress === "string" && formData.ceremonyStreetAddress.length > 2)) {
+            errors.ceremonyStreetAddress = "Enter ceremony street address, use at least 2 characters"
+        }
+        if(!("ceremonyCityAddress" in formData && typeof formData.ceremonyCityAddress === "string" && formData.ceremonyCityAddress.length > 2)) {
+            errors.ceremonyCityAddress = "Enter ceremony city address, use at least 2 characters"
         }
         if(!("receptionPlace" in formData && typeof formData.receptionPlace === "string" && formData.receptionPlace.length > 2)) {
-            errors.receptionPlace = 'Enter reception place, use at least 2 characters'
+            errors.receptionPlace = "Enter reception place, use at least 2 characters"
         }
-        if(!("receptionAddress" in formData && typeof formData.receptionAddress === "string" && formData.receptionAddress.length > 2)) {
-            errors.receptionAddress = 'Enter reception address, use at least 2 characters'
+        if(!("receptionStreetAddress" in formData && typeof formData.receptionStreetAddress === "string" && formData.receptionStreetAddress.length > 2)) {
+            errors.receptionStreetAddress = "Enter reception street address, use at least 2 characters"
+        }
+        if(!("receptionCityAddress" in formData && typeof formData.receptionCityAddress === "string" && formData.receptionCityAddress.length > 2)) {
+            errors.receptionCityAddress = "Enter reception city address, use at least 2 characters"
         }
         if(!("firstPersonPhone" in formData && typeof formData.firstPersonPhone === "string" && formData.firstPersonPhone.length >= 6)) {
             errors.firstPersonPhone = "Enter first person's number. Use at least 6 numbers"
@@ -88,107 +96,105 @@ export default function NewEventPage () {
     }
 
     return (
-        <Card className='w-full max-w-screen-lg'>
+        <Card className="w-full max-w-screen-lg">
             <CardHeader>
-                <CardTitle className='text-center'>Your dream event</CardTitle>
+                <CardTitle className="text-center">Your dream event</CardTitle>
             </CardHeader>
             <CardContent>
-                <form id='EventForm' onSubmit={handleOnSubmit}>
-                    <div className='w-full'>
-                        <div className='text-lg font-bold mb-2'><p>Names</p>
-                            <div className='grid grid-cols-2 gap-4'>
-                                <div className='flex flex-col space-y-1.5 mb-5'>
-                                    <Label>First person 
-                                        <Input name='firstPerson'/>
-                                        {!!error?.firstPerson && <em>{error.firstPerson}</em>}
-                                    </Label>
+                <form id="EventForm" onSubmit={handleOnSubmit}>
+                    <div className="w-full">
+
+                        <div className="text-lg font-bold mb-2"><p className="border-t-2 py-4 mb-4">Names</p>
+                            <div className="grid grid-cols-3 gap-4">
+                                <div className="col-start-2 flex flex-col space-y-1.5 mb-5">
+                                    <Label htmlFor="firstPerson" >First person</Label> 
+                                    <Input name="firstPerson"/>
+                                    {!!error?.firstPerson && <em className="text-xs">{error.firstPerson}</em>}
                                 </div>
-                                <div className='flex flex-col space-y-1.5 mb-5'>
-                                    <Label>Second person 
-                                        <Input name='secondPerson'/>
-                                        {!!error?.secondPerson && <em>{error.secondPerson}</em>}
-                                    </Label>
+                                <div className="flex flex-col space-y-1.5 mb-5">
+                                    <Label htmlFor="secondPerson">Second person</Label> 
+                                    <Input name="secondPerson"/>
+                                    {!!error?.secondPerson && <em className="text-xs">{error.secondPerson}</em>} 
                                 </div>
                             </div>
                         </div>
                         
-
-                        <div className='text-lg font-bold mb-2'><p>Event</p>
-                            <div className='grid grid-cols-2 gap-4'>
-                                <div className='flex flex-col space-y-1.5 mb-5'>
-                                    <Label>Date 
-                                    <DatePicker 
-                                            value={eventDate}
-                                            onSelectDate={(date) => setEventDate(date)}
-                                        />
-                                    </Label>
+                        <div className="text-lg font-bold mb-2"><p className="border-t-2 py-4 mb-4">Event</p>
+                            <div className="grid grid-cols-3 gap-4">
+                                <div className="col-start-2 flex flex-col space-y-1.5 mb-5">
+                                    <Label>Date</Label> 
+                                    <DatePicker value={eventDate} onSelectDate={(date) => setEventDate(date)} />
+                                    {!!error?.eventDate && <em className="text-xs">{error.eventDate}</em>}                                   
                                 </div>
-                                <div className='flex flex-col space-y-1.5 mb-5'>
-                                    <Label>Time 
-                                        <Input name='eventTime' type='time'/>
-                                        {!!error?.eventTime && <em>{error.eventTime}</em>}
-                                    </Label>
+                                <div className="flex flex-col space-y-1.5 mb-5">
+                                    <Label htmlFor="eventTime">Time</Label> 
+                                    <Input name="eventTime" type="time"/>
+                                    {!!error?.eventTime && <em className="text-xs">{error.eventTime}</em>}
                                 </div>
                             </div>
                         </div>
 
-                        <div className='text-lg font-bold mb-2'><p>Ceremony</p>
-                            <div className='grid grid-cols-2 gap-4'>
-                                <div className='flex flex-col space-y-1.5 mb-5'>
-                                    <Label>Place 
-                                        <Input name='ceremonyPlace' />
-                                        {!!error?.ceremonyPlace && <em>{error.ceremonyPlace}</em>}
-                                    </Label>
+                        <div className="text-lg font-bold mb-2"><p className="border-t-2 py-4 mb-4">Ceremony</p>
+                            <div className="grid grid-cols-3 gap-4">
+                                <div className="col-start-2 flex flex-col space-y-1.5 mb-5">
+                                    <Label htmlFor="ceremonyPlace">Place</Label> 
+                                    <Input name="ceremonyPlace" />
+                                    {!!error?.ceremonyPlace && <em className="text-xs">{error.ceremonyPlace}</em>}
+                                    
                                 </div>
-                                <div className='flex flex-col space-y-1.5 mb-5'>
-                                    <Label>Address 
-                                        <Textarea name='ceremonyAddress' />
-                                        {!!error?.ceremonyAddress && <em>{error.ceremonyAddress}</em>}
-                                    </Label>
+                                <div className="flex flex-col space-y-1.5 mb-5">
+                                    <Label htmlFor="ceremonyStreetAddress">Street</Label> 
+                                    <Input name="ceremonyStreetAddress" />
+                                    {!!error?.ceremonyStreetAddress && <em className="text-xs">{error.ceremonyStreetAddress}</em>}
                                 </div>
-                            </div>
-                        </div>
-
-                        <div className='text-lg font-bold mb-2'><p>Reception</p>
-                            <div className='grid grid-cols-2 gap-4'>
-                                <div className='flex flex-col space-y-1.5 mb-5'>
-                                    <Label>Place 
-                                        <Input name='receptionPlace' />
-                                        {!!error?.receptionPlace && <em>{error.receptionPlace}</em>}
-                                    </Label>
-                                </div>
-                                <div className='flex flex-col space-y-1.5 mb-5'>
-                                    <Label>Address 
-                                        <Textarea name='receptionAddress' />
-                                        {!!error?.receptionAddress && <em>{error.receptionAddress}</em>}
-                                    </Label>
+                                <div className="col-start-3 flex flex-col space-y-1.5 mb-5">
+                                    <Label htmlFor="ceremonyCityAddress">City</Label> 
+                                    <Input name="ceremonyCityAddress" />
+                                    {!!error?.ceremonyCityAddress && <em className="text-xs">{error.ceremonyCityAddress}</em>}
                                 </div>
                             </div>
                         </div>
 
-                        <div className='text-lg font-bold mb-2'><p>Phone numbers</p>
-                            <div className='grid grid-cols-2 gap-4'>
-                                <div className='flex flex-col space-y-1.5 mb-5'>
-                                    <Label>First person 
-                                        <Input name='firstPersonPhone' type='tel' />
-                                        {!!error?.firstPersonPhone && <em>{error.firstPersonPhone}</em>}
-                                    </Label>
+                        <div className="text-lg font-bold mb-2"><p className="border-t-2 py-4 mb-4">Reception</p>
+                            <div className="grid grid-cols-3 gap-4">
+                                <div className="col-start-2 flex flex-col space-y-1.5 mb-5">
+                                    <Label htmlFor="receptionPlace">Place</Label>
+                                    <Input name="receptionPlace" />
+                                    {!!error?.receptionPlace && <em className="text-xs">{error.receptionPlace}</em>}
                                 </div>
-                                <div className='flex flex-col space-y-1.5 mb-5'>
-                                    <Label>Second person
-                                        <Input name='secondPersonPhone' type='tel' />
-                                        {!!error?.secondPersonPhone && <em>{error.secondPersonPhone}</em>}
-                                    </Label>
+                                <div className="flex flex-col space-y-1.5 mb-5">
+                                    <Label htmlFor="receptionStreetAddress">Street</Label> 
+                                    <Input name="receptionStreetAddress" />
+                                    {!!error?.receptionStreetAddress && <em className="text-xs">{error.receptionStreetAddress}</em>}
+                                </div>
+                                <div className="col-start-3 flex flex-col space-y-1.5 mb-5">
+                                    <Label htmlFor="receptionCityAddress">City</Label> 
+                                    <Input name="receptionCityAddress" />
+                                    {!!error?.receptionCityAddress && <em className="text-xs">{error.receptionCityAddress}</em>}
                                 </div>
                             </div>
                         </div>
 
-                        <div className='text-lg font-bold mb-2'><p>Other</p>
-                            <div className='grid grid-cols-2 gap-4'>
-                                <div className='flex flex-col space-y-1.5 mb-5'>
-                                    <Label>Lead color 
-                                        <Input name='color' type='color' />
-                                    </Label>
+                        <div className="text-lg font-bold mb-2"><p className="border-t-2 py-4 mb-4">Phone numbers</p>
+                            <div className="grid grid-cols-3 gap-4">
+                                <div className="col-start-2 flex flex-col space-y-1.5 mb-5">
+                                    <Label htmlFor="firstPersonPhone">First person</Label>
+                                    <Input name="firstPersonPhone" type="tel" />
+                                    {!!error?.firstPersonPhone && <em className="text-xs">{error.firstPersonPhone}</em>}
+                                </div>
+                                <div className="flex flex-col space-y-1.5 mb-5">
+                                    <Label htmlFor="econdPersonPhone">Second person</Label>
+                                    <Input name="secondPersonPhone" type="tel" />
+                                    {!!error?.secondPersonPhone && <em className="text-xs">{error.secondPersonPhone}</em>}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="text-lg font-bold mb-2"><p className="border-t-2 py-4 mb-4">Other</p>
+                            <div className="grid grid-cols-3 gap-4">
+                                <div className="col-start-2 col-end-4 flex flex-col space-y-1.5 mb-5">
+                                    <Label htmlFor="color">You can choose lead color of your event</Label> 
+                                    <Input name="color" type="color" />
                                 </div>
                             </div>
                         </div>
@@ -196,9 +202,9 @@ export default function NewEventPage () {
                     </div>
                 </form>
             </CardContent>
-            <CardFooter className='grid grid-cols-2 gap-4'>
-                <Button className='w-full'>Cancel</Button>
-                <Button type='submit' form='EventForm' className='w-full'>Add your event</Button>
+            <CardFooter className="grid grid-cols-3 gap-4 border-t-2 py-8">
+                <Button className="col-start-2" variant="outline">Cancel</Button>
+                <Button type="submit" form="EventForm" >Add your event</Button>
             </CardFooter>
         </Card>
     )
