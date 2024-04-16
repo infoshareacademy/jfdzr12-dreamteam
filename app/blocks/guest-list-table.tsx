d09acc2f56d841d78491ffc3173344f1632a3185
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-04-17 00:12:47 +0200

    added action delete

--- app/blocks/guest-list-table.tsx
+++ app/blocks/guest-list-table.tsx
@@ -1,26 +1,24 @@
 import { collection, getDocs } from 'firebase/firestore'
 import { db } from '~/db/firebase'
 import { useEffect, useState } from 'react';
-import { Table, TableBody, TableRow, TableCell, TableHead, TableHeader, TableCaption } from '~/atoms/ui/table';
+import { Table, TableBody, TableRow, TableCell, TableHead, TableHeader } from '~/atoms/ui/table';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/atoms/ui/card";
-import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '~/atoms/ui/dropdown-menu';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from '~/atoms/ui/dropdown-menu';
 import { Button } from '~/atoms/ui/button';
 import { MoreHorizontal } from 'lucide-react';
 
-
-
 export const GuestListTable = () => {
-  const [guests, setGuests] = useState([]);
+  const [guests, setGuests] = useState([{}]);
 
   const getGuestList = async () => {
     const guestListCollection = collection(db, 'guestlist');
     try {
       const querySnapshot = await getDocs(guestListCollection);
-      const list = querySnapshot.docs.map(doc => ({
+      const guestList = querySnapshot.docs.map(doc => ({
         id: doc.id,
         ...doc.data()
       }));
-      setGuests(list);
+      setGuests(guestList);
     } catch (error) {
       console.error('Error getting guest list: ', error);
     }
@@ -29,6 +27,12 @@ export const GuestListTable = () => {
   useEffect(() => {
     getGuestList();
   }, []);
+
+  const handleDelete = (id) => {
+    const updatedGuests = guests.filter(guest => guest.id !== id);
+    setGuests(updatedGuests);
+  }
+
 
   return (
     <Card className="w-fit" x-chunk="dashboard-06-chunk-0">
@@ -69,7 +73,7 @@ export const GuestListTable = () => {
                     <DropdownMenuContent align="end">
                       <DropdownMenuLabel>Actions</DropdownMenuLabel>
                       <DropdownMenuItem>Edit</DropdownMenuItem>
-                      <DropdownMenuItem>Delete</DropdownMenuItem>
+                      <DropdownMenuItem onClick={() => handleDelete(guest.id)}>Delete</DropdownMenuItem>
                     </DropdownMenuContent>
                   </DropdownMenu>
                 </TableCell>
@@ -77,46 +81,6 @@ export const GuestListTable = () => {
             ))}
           </TableBody>
         </Table>
-        {/* <Table>
-          <TableHead>
-            <TableRow>
-              <TableHead>First Name</TableHead>
-              <TableHead>Last Name</TableHead>
-              <TableHead>Email</TableHead>
-              <TableHead>
-                <span className="sr-only">Actions</span>
-              </TableHead>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {guests.map(guest => (
-              <TableRow key={guest.id}>
-                <TableCell className="font-medium">{guest.firstName}</TableCell>
-                <TableCell className="font-medium">{guest.lastName}</TableCell>
-                <TableCell className="font-medium">{guest.email}</TableCell>
-                <TableCell>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Button
-                        aria-haspopup="true"
-                        size="icon"
-                        variant="ghost"
-                      >
-                        <MoreHorizontal className="h-4 w-4" />
-                        <span className="sr-only">Toggle menu</span>
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                      <DropdownMenuItem>Edit</DropdownMenuItem>
-                      <DropdownMenuItem>Delete</DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table> */}
       </CardContent>
       <CardFooter>
         <div className="text-xs text-muted-foreground">
