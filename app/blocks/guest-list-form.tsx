fd07a7d4a10c95919366fe3c323b067b773b6dd3
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-04-16 23:57:03 +0200

    guest list form validation is working!!!

--- app/blocks/guest-list-form.tsx
+++ app/blocks/guest-list-form.tsx
@@ -19,79 +19,38 @@ const newGuestListData = collection(db, 'guestlist');
 
 export const GuestListForm = () => {
   const [errors, setErrors] = useState<FormErrorData<NewGuest>>({});
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [submitting, setSubmitting] = useState(false);
-
-  const isFormInvalid = !firstName || !lastName || !email;
 
   async function handleOnSubmit(e: FormEvent) {
     if (!(e.target instanceof HTMLFormElement)) {
       return;
     }
-
     e.preventDefault();
-
-    setSubmitting(true);
-
     const _formData = new FormData(e.target);
-
+    console.log("_formData", _formData);
     const formData = Object.fromEntries(_formData.entries());
-
+    console.log("submit", formData);
     const errors: FormErrorData<NewGuest> = {};
 
-    const validate = () => {
-      let hasErrors = false;
+    if (!("firstName" in formData && typeof formData.firstName !== "string" || formData.firstName.length >= 2)) {
+      errors.firstName = 'First name is required and must be at least 2 characters';
+    }
 
-      if (!firstName) {
-        errors.firstName = 'First name required';
-        hasErrors = true;
-      } else if (!("lastName" in formData && typeof formData.lastName === "string" && formData.lastName.length > 2)) {
-        errors.lastName = "Full surname is required";
-        hasErrors = true;
-      }
+    if (!("lastName" in formData && typeof formData.lastName !== "string" || formData.lastName.length >= 2)) {
+      errors.lastName = 'Last name is required and must be at least 2 characters';
+    }
 
-      if (!lastName) {
-        errors.lastName = 'Last name required';
-        hasErrors = true;
-      } else if (!("firstName" in formData && typeof formData.firstName === "string" && formData.firstName.length > 2)) {
-        errors.firstName = "Full name is required";
-        hasErrors = true;
-      }
+    if (!("email" in formData && typeof formData.email !== "string" && /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formData.email))) {
+      errors.email = 'Email is required';
+    }
 
-      if (!email) {
-        errors.email = 'Email required';
-        hasErrors = true;
-      } else if (!("email" in formData && typeof formData.email === "string" && formData.email.includes("@"))) {
-        errors.email = "Invalid email format";
-        hasErrors = true;
-      }
-
+    if (Object.keys(errors).length !== 0) {
+      e.target.reset();
       setErrors(errors);
-      return hasErrors;
+      return;
+    } else {
+      addDoc(newGuestListData, formData);
+      e.target.reset();
     }
-
-    if (submitting) {
-      const hasErrors = validate();
-      if (hasErrors) {
-        return;
-      }
-    }
-
-    addDoc(newGuestListData, formData);
-
-    setFirstName('');
-    setLastName('');
-    setEmail('');
-    setErrors({});
-  }
-
-  const resetValues = () => {
-    setFirstName('');
-    setLastName('');
-    setEmail('');
-    setSubmitting(false);
   }
 
   return (
@@ -103,23 +62,23 @@ export const GuestListForm = () => {
         <CardContent className=" grid grid-cols-3 gap-6 items-top">
           <div className="space-y-1">
             <Label htmlFor="firstName">First name</Label>
-            <Input name="firstName" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="First name" />
-            {submitting && !!errors?.firstName && <em className="text-xs">{errors.firstName}</em>}
+            <Input name="firstName" type="text" placeholder="First name" />
+            {!!errors?.firstName && <em className="text-xs">{errors.firstName}</em>}
           </div>
           <div className="space-y-1">
             <Label htmlFor="lastName">Last name</Label>
-            <Input name="lastName" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Last name" />
-            {submitting && !!errors?.lastName && <em className="text-xs">{errors.lastName}</em>}
+            <Input name="lastName" type="text" placeholder="Last name" />
+            {!!errors?.lastName && <em className="text-xs">{errors.lastName}</em>}
           </div>
           <div className="space-y-1">
             <Label htmlFor="email">Email address</Label>
-            <Input name="email" type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-            {submitting && !!errors?.email && <em className="text-xs">{errors.email}</em>}
+            <Input name="email" type="text" placeholder="Email" />
+            {!!errors?.email && <em className="text-xs">{errors.email}</em>}
           </div>
         </CardContent>
         <CardFooter className='grid grid-cols-2 gap-4'>
-          <Button variant='outline' className='w-full' onClick={resetValues}>Cancel</Button>
-          <Button type='submit' form='GuestListForm' className='w-full' disabled={isFormInvalid}>Add your guest</Button>
+          <Button variant='outline' className='w-full' >Cancel</Button>
+          <Button type='submit' form='GuestListForm' className='w-full' >Add your guest</Button>
         </CardFooter>
       </Card>
     </form>
