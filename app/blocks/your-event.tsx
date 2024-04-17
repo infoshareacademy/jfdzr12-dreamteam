ec5acd134c889f9ccf1251511e4ec0ca17176a6e
Monika Pławska <monikaplawska88@gmail.com>
2024-04-17 14:40:18 +0200

    your event data

--- app/blocks/your-event.tsx
+++ app/blocks/your-event.tsx
@@ -1,22 +1,87 @@
+import { useEffect, useState } from "react";
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "~/atoms/ui/resizable";
+import { getYourEvent } from "~/db/get-your-event";
+
+interface EventData {
+    firstPerson: string,
+    secondPerson: string,
+    eventDate: string,
+    eventTime: string,
+    ceremonyPlace: string,
+    ceremonyStreetAddress: string,
+    ceremonyCityAddress: string,
+    receptionPlace: string,
+    receptionStreetAddress: string,
+    receptionCityAddress: string,
+    firstPersonPhone: string,
+    secondPersonPhone: string,
+    eventID: string,
+    color: string,
+}
 
 export default function YourEvent() {
+    const [eventData, setEventData] = useState<EventData>();
+
+    async function getEventData() {
+        const data = await getYourEvent();
+        setEventData(data as EventData);
+    }
+
+    useEffect(() => {
+        getEventData();
+    }, []);
+
+    let content;
+
+    if(eventData) {
+        const today: Date = new Date();
+        const eventDate: Date = new Date(eventData.eventDate);
+        const timeDifference: number = eventDate.getTime() - today.getTime();
+
+        if(timeDifference < 0) {
+            content = "Wydarzenie już się odbyło";
+        } else {
+            const numberOfDaysToEvent = Math.floor(timeDifference / (1000 * 3600 * 24));
+            content = numberOfDaysToEvent;
+        }
+    } 
+    
+
     return (
-                <ResizablePanelGroup
-                    direction="vertical"
-                    className="min-h-[200px] max-w-md rounded-lg border"
-                >
-                    <ResizablePanel defaultSize={25}>
-                    <div className="flex h-full items-center justify-center p-6">
-                        <span className="font-semibold">Your wedding is in...</span>
+        <>
+        {eventData && (
+            <ResizablePanelGroup
+                direction="horizontal"
+                className="max-w-md rounded-lg border"
+            >
+                <ResizablePanel defaultSize={100}>
+                    <div className="flex h-[200px] items-center justify-center p-6">
+                        <span className="font-semibold">{content}</span>
+                        <span className="font-semibold">{eventData.eventTime}</span>
                     </div>
+                </ResizablePanel>
+                <ResizableHandle />
+                <ResizablePanel defaultSize={50}>
+                    <ResizablePanelGroup direction="vertical">
+                    <ResizablePanel defaultSize={25}>
+                        <div className="flex h-full items-center justify-center p-6">
+                            <p className="font-semibold">{eventData.ceremonyPlace}</p>
+                            <p className="font-semibold">{eventData.ceremonyStreetAddress}</p>
+                            <p className="font-semibold">{eventData.ceremonyCityAddress}</p>
+                        </div>
                     </ResizablePanel>
                     <ResizableHandle />
                     <ResizablePanel defaultSize={75}>
-                    <div className="flex h-full items-center justify-center p-6">
-                        <span className="font-semibold">...</span>
-                    </div>
+                        <div className="flex h-full items-center justify-center p-6">
+                            <p className="font-semibold">{eventData.receptionPlace}</p>
+                            <p className="font-semibold">{eventData.receptionStreetAddress}</p>
+                            <p className="font-semibold">{eventData.receptionCityAddress}</p>
+                        </div>
                     </ResizablePanel>
-                </ResizablePanelGroup>
-            )
+                    </ResizablePanelGroup>
+                </ResizablePanel>
+            </ResizablePanelGroup>
+        )}
+        </>
+    );
 }
---
+++ app/db/get-your-event.ts
@@ -0,0 +1,16 @@
+import { getDocs, query, where } from "firebase/firestore";
+import { eventRef } from "./event-ref";
+
+const eventIdQuery = query(eventRef, where("eventID", "==", "1013"));
+
+export async function getYourEvent() {
+    const querySnapshot = await getDocs(eventIdQuery);
+    if(querySnapshot.empty) {
+        console.log("No events");
+    } else {
+        const lastEvent = querySnapshot.docs[0];
+        const lastEventData = lastEvent.data();
+        console.log("event data", lastEventData)
+        return lastEventData;
+    }
+}
