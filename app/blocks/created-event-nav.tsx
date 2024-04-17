9ad9c0c63db8f34d78217c016e122db7f77f4740
Aleksandra Boroń <alek166194@wgmail.com>
2024-04-17 19:45:04 +0200

    app/blocks/created-event-nav.tsx

---
+++ app/blocks/created-event-nav.tsx
@@ -0,0 +1,57 @@
+// import { collection } from "firebase/firestore";
+// import { db } from "~/db/firebase";
+// export const eventRef = collection(db, 'event');
+
+// export const eventIdref = collection(db, 'eventID');
+
+// import { getDocs, query, where } from "firebase/firestore";
+
+// const eventIdQuery = query(eventRef, where("eventID", "==", "1016"));
+
+// export async function getYourEvent() {
+//     const querySnapshot = await getDocs(eventIdQuery);
+//     if(querySnapshot.empty) {
+//         console.log("No events");
+//     } else {
+//         const lastEvent = querySnapshot.docs[0];
+//         const lastEventData = lastEvent.data();
+//         console.log("event data", lastEventData)
+//         return lastEventData;
+//     }
+// }
+
+
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Button } from '~/atoms/ui/button';
+// import { getYourEvent } from './event-api';; , jak Monia doda funkcję to ścieżka nowa będzie
+
+export const CreatedEventNav = () => {
+    const [brideName, setBrideName] = useState('');
+    const [groomName, setGroomName] = useState('');
+
+    useEffect(() => {
+        const fetchEvent = async () => {
+            try {
+                const eventData = await getYourEvent();
+                if (eventData) {
+                    setBrideName(eventData.firstPerson);
+                    setGroomName(eventData.secondPerson);
+                }
+            } catch (error) {
+                console.error('Error fetching event data:', error);
+            }
+        };
+
+        fetchEvent();
+    }, []);
+
+    return (
+        <div className="flex justify-center items-center h-64">
+            <NavLink to="/your-event">
+                <Button>{brideName} & {groomName}</Button>
+            </NavLink>
+        </div>
+    );
+};
+
--- app/routes/add-event.tsx
+++ app/routes/add-event.tsx
@@ -1,5 +1,26 @@
-import { AddEvent } from "~/blocks/add-event"
-export default function SignIpPage (){
-return (
-<AddEvent/>
-)}
+// import { AddEvent } from "~/blocks/add-event"
+// import {CreatedEventNav} from "~/blocks/created-event-nav"
+// export default function SignIpPage (){
+// return (
+//     <>
+// <AddEvent/>
+// <CreatedEventNav/>
+// </>
+// )}
+
+
+import React from 'react';
+import { AddEvent } from '~/blocks/add-event';
+import { CreatedEventNav } from '~/blocks/created-event-nav';
+
+export default function SignInPage() {
+  return (
+   <div> 
+      <AddEvent />
+      <CreatedEventNav />
+    </div>
+  );
+}
+
+
+
