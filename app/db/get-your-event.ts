e43683ccc83a97b023ffef7b2ac8d01960c209d7
Monika Pławska <monikaplawska88@gmail.com>
2024-04-18 22:10:14 +0200

    get-your-event function test

--- app/db/get-your-event.ts
+++ app/db/get-your-event.ts
@@ -1,16 +1,27 @@
 import { getDocs, query, where } from "firebase/firestore";
 import { eventRef } from "./event-ref";
-
-const eventIdQuery = query(eventRef, where("eventID", "==", "1016"));
+import { getUserUID } from "./get-user-uid";
+import { auth } from "./firebase";
 
 export async function getYourEvent() {
-    const querySnapshot = await getDocs(eventIdQuery);
-    if(querySnapshot.empty) {
-        console.log("No events");
-    } else {
-        const lastEvent = querySnapshot.docs[0];
-        const lastEventData = lastEvent.data();
-        console.log("event data", lastEventData)
-        return lastEventData;
+        const currentUserUID = await getUserUID();
+        if (!currentUserUID) {
+        console.log('Użytkownik nie jest zalogowany');
+        return;
+    } else {   
+        const eventQuery = query(eventRef, where("userUID", "==", currentUserUID));
+        console.log('current uid from get your event', currentUserUID)
+        const querySnapshot = await getDocs(eventQuery);
+        if(querySnapshot.empty) {
+            console.log("No events");
+        } else {
+            const lastEventData = querySnapshot.docs[0].data();
+            console.log("event data", lastEventData)
+            return lastEventData;
+        }
     }
 }
+
+console.log(auth)
+
+
