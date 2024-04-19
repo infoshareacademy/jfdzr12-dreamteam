81a9c300f581c805a06f1c97ff060e99c0236a62
Monika Pławska <monikaplawska88@gmail.com>
2024-04-19 16:01:18 +0200

    className on Link to add-event added, cancel button deleted

--- app/routes/new-event.tsx
+++ app/routes/new-event.tsx
@@ -227,9 +227,13 @@ export default function NewEventPage() {
                 </form>
             </CardContent>
             <CardFooter className="grid grid-cols-3 gap-4">
-                <Button className="col-start-2" variant="outline">
+                {/* <Button className="col-start-2" variant="outline">
                     <Link to="/add-event">Cancel</Link>
-                </Button>
+                </Button> */}
+                <Link to="/add-event" className="col-start-2 border border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2" >
+                    Cancel
+                </Link>
+                {/* nie wiem jak po kliknięciu i wysłaniu formularza przekierować użytkownika na stronę z wydarzeniami */}
                 <Button type="submit" form="EventForm" >Add your event</Button>
             </CardFooter>
         </Card>
