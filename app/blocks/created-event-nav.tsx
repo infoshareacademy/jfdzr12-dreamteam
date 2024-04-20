4411961e5dff3a5e331c946e1302c4238545ab54
Aleksandra Boroń <alek166194@wgmail.com>
2024-04-20 20:43:28 +0200

    modified:   app/blocks/created-event-nav.tsx

--- app/blocks/created-event-nav.tsx
+++ app/blocks/created-event-nav.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { Button } from '~/atoms/ui/button';
 import { getYourEvent } from '~/db/get-your-event';
 import { eventRef } from '~/db/event-ref';
+import { deleteDoc, doc } from "firebase/firestore";
+import { getUserUID } from '~/db/get-user-uid';
 
 export const CreatedEventNav = () => {
     const [brideName, setBrideName] = useState('');
     const [groomName, setGroomName] = useState('');
     const [eventExists, setEventExists] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchEvent = async () => {
@@ -19,20 +22,34 @@ export const CreatedEventNav = () => {
                     setEventExists(true); 
                 }
             } catch (error) {
-                console.error('Error retrieving event data:', error);
+                console.error('Error retrieving event data', error);
             }
         };
 
         fetchEvent();
     }, []);
 
-    const handleDelete = () => {
+    const handleDelete = async () => {
         const createdEventNav = document.getElementById('created-event-nav');
         if (createdEventNav) {
             createdEventNav.remove();
+    
+            try {
+                const currentUserUID = await getUserUID();
+                if (currentUserUID) {
+                    await deleteEventData(currentUserUID);
+                    console.log('The event data has been successfully deleted');
+                    navigate('/add-event');
+                }
+            } catch (error) {
+                console.error('Error deleting event data', error);
+            }
         }
     };
 
+   const deleteEventData = async (currentUserUID: string): Promise<void> => {
+        await deleteDoc(doc(eventRef, currentUserUID));
+    };
     return (
         <div className="flex justify-center items-center h-64" id="created-event-nav">
             {eventExists ? ( 
