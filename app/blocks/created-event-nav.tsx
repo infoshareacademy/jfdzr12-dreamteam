ee3edef673889e3141a97db579d24bbb0b55eb8c
Aleksandra Boroń <alek166194@wgmail.com>
2024-04-20 20:52:58 +0200

    app/blocks/created-event-nav.tsx

--- app/blocks/created-event-nav.tsx
+++ app/blocks/created-event-nav.tsx
@@ -13,58 +13,49 @@ export const CreatedEventNav = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        const fetchEvent = async () => {
-            try {
-                const eventData = await getYourEvent();
+        getYourEvent()
+            .then((eventData) => {
                 if (eventData) {
                     setBrideName(eventData.firstPerson);
                     setGroomName(eventData.secondPerson);
                     setEventExists(true); 
                 }
-            } catch (error) {
+            })
+            .catch((error) => {
                 console.error('Error retrieving event data', error);
-            }
-        };
-
-        fetchEvent();
+            });
     }, []);
 
     const handleDelete = async () => {
         const createdEventNav = document.getElementById('created-event-nav');
         if (createdEventNav) {
             createdEventNav.remove();
-    
-            try {
-                const currentUserUID = await getUserUID();
-                if (currentUserUID) {
-                    await deleteEventData(currentUserUID);
-                    console.log('The event data has been successfully deleted');
-                    navigate('/add-event');
-                }
-            } catch (error) {
-                console.error('Error deleting event data', error);
-            }
+            getUserUID()
+                .then(async (currentUserUID) => {
+                    if (currentUserUID) {
+                        await deleteEventData(currentUserUID);
+                        console.log('The event data has been successfully deleted');
+                        navigate('/add-event');
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error deleting event data', error);
+                });
         }
     };
 
-   const deleteEventData = async (currentUserUID: string): Promise<void> => {
+    const deleteEventData = async (currentUserUID: string): Promise<void> => {
         await deleteDoc(doc(eventRef, currentUserUID));
     };
+
     return (
         <div className="flex justify-center items-center h-64" id="created-event-nav">
             {eventExists ? ( 
                 <div className="flex items-center">
-                    
                     <NavLink to="/your-event">
                         <Button>{brideName} & {groomName}</Button>
                     </NavLink>
-                    
-                    <button
-                        className="ml-2 p-1 text-gray-700 rounded-full"
-                        onClick={handleDelete}
-                    >
-                        X
-                    </button>
+                    <button className="ml-2 p-1 text-gray-700 rounded-full" onClick={handleDelete}> X </button>
                 </div>
             ) : (
                 <p>No events</p>
