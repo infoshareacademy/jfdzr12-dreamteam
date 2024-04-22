47e09fe6abf173252a2d2a8a74e56306a3f4d3af
Monika Pławska <monikaplawska88@gmail.com>
2024-04-22 13:45:58 +0200

    date picker modified

--- app/atoms/ui/date-picker.tsx
+++ app/atoms/ui/date-picker.tsx
@@ -13,12 +13,10 @@ import {
 interface DatePickerProps {
     value: Date | undefined;
     onSelectDate: (date: Date) => void;
-    resetDate?: boolean;
 }
 
-export function DatePicker({value, onSelectDate, resetDate}: DatePickerProps) {
+export function DatePicker({value, onSelectDate}: DatePickerProps) {
     const [date, setDate] = useState<Date>();
-    const [isOpen, setIsOpen] = useState<boolean>(false);
 
     useEffect(() => {
         if (value) {
@@ -26,17 +24,10 @@ export function DatePicker({value, onSelectDate, resetDate}: DatePickerProps) {
         }
     }, [value]);
 
-    useEffect(() => {
-        if (resetDate) {
-            setDate(undefined);
-        }
-    }, [resetDate]);
-
     const handleDateOnChange = (selectedDate: Date | undefined) => {
         if(selectedDate) {
             onSelectDate(selectedDate);
             setDate(selectedDate);
-            setIsOpen(false);
         }
     }
 
@@ -49,21 +40,18 @@ export function DatePicker({value, onSelectDate, resetDate}: DatePickerProps) {
                     'justify-start text-left font-normal',
                     !date && 'text-muted-foreground'
                 )}
-                onClick={() => setIsOpen(!isOpen)}
                 >
                 <CalendarIcon className='mr-2 h-4 w-4' />
                 {date ? format(date, 'PPP') : <span>Pick a date</span>}
                 </Button>
             </PopoverTrigger>
             <PopoverContent className='w-auto p-0'>
-            {isOpen &&
                 <Calendar
                 mode='single'
                 selected={date}
                 onSelect={handleDateOnChange}
                 initialFocus
                 />
-            }
             </PopoverContent>
             </Popover>
         )
