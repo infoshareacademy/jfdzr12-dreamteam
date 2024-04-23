e112d93c8fca53b7085eca8d45182533cc1a2c07
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-04-23 13:31:57 +0200

    added tabs

--- app/blocks/guest-list-table.tsx
+++ app/blocks/guest-list-table.tsx
@@ -8,6 +8,7 @@ import { Button } from '~/atoms/ui/button';
 import { MoreHorizontal } from 'lucide-react';
 import { NewGuest } from '~/type/new-guest';
 import { guestRef } from '~/db/guest-list-ref';
+import { Tabs, TabsContent, TabsList, TabsTrigger, } from '~/atoms/ui/tabs';
 
 export const GuestListTable = () => {
   const [guests, setGuests] = useState<NewGuest[]>([]);
@@ -43,52 +44,114 @@ export const GuestListTable = () => {
     <Card className="w-9/12 mt-5 mb-6 mx-auto dashboard-06-chunk-0">
       <CardHeader>
         <CardTitle>Guest list</CardTitle>
-        <CardDescription>Manage your guests and check their survey responses.</CardDescription>
+        <CardDescription>Manage your guests and check their preferences.</CardDescription>
       </CardHeader>
-      <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>ID</TableHead>
-              <TableHead>First Name</TableHead>
-              <TableHead>Last Name</TableHead>
-              <TableHead>Email</TableHead>
-              <TableHead>
-                <span className="sr-only">Actions</span>
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {guests.map(guest => (
-              <TableRow key={guest.id}>
-                <TableCell className="font-medium">{guest.guestID}</TableCell>
-                <TableCell className="font-medium">{guest.firstName}</TableCell>
-                <TableCell className="font-medium">{guest.lastName}</TableCell>
-                <TableCell className="font-medium">{guest.email}</TableCell>
-                <TableCell>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Button
-                        aria-haspopup="true"
-                        size="icon"
-                        variant="ghost"
-                      >
-                        <MoreHorizontal className="h-4 w-4" />
-                        <span className="sr-only">Toggle menu</span>
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                      {/* <DropdownMenuItem>Edit</DropdownMenuItem> */}
-                      <DropdownMenuItem onClick={() => handleDelete(guest.id)}>Delete</DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </CardContent>
+      <Tabs defaultValue="guests">
+        <TabsList className="grid mx-6 grid-cols-2">
+          <TabsTrigger value="guests">Guest list</TabsTrigger>
+          <TabsTrigger value="preferences">Preferences</TabsTrigger>
+        </TabsList>
+        <TabsContent value="guests">
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>ID</TableHead>
+                  <TableHead>Name</TableHead>
+                  {/* <TableHead>Last Name</TableHead> */}
+                  <TableHead>Email</TableHead>
+                  <TableHead>Confirmation</TableHead>
+                  <TableHead>
+                    <span /*className="sr-only"*/>Actions</span>
+                  </TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {guests.map(guest => (
+                  <TableRow key={guest.id}>
+                    <TableCell className="font-medium">{guest.guestID}</TableCell>
+                    <TableCell className="font-medium">{guest.firstName} {guest.lastName}</TableCell>
+                    {/* <TableCell className="font-medium">{guest.lastName}</TableCell> */}
+                    <TableCell className="font-medium">{guest.email}</TableCell>
+                    <TableCell>test</TableCell>
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button
+                            aria-haspopup="true"
+                            size="icon"
+                            variant="ghost"
+                          >
+                            <MoreHorizontal className="h-4 w-4" />
+                            <span className="sr-only">Toggle menu</span>
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                          {/* <DropdownMenuItem>Edit</DropdownMenuItem> */}
+                          <DropdownMenuItem onClick={() => handleDelete(guest.id)}>Delete</DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </TabsContent>
+        <TabsContent value="preferences">
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="p-2">ID</TableHead>
+                  <TableHead className="p-2">Name</TableHead>
+                  <TableHead className="p-2">Test</TableHead>
+                  <TableHead className="p-2">Test</TableHead>
+                  <TableHead className="p-2">Test</TableHead>
+                  <TableHead className="p-2">Test</TableHead>
+                  <TableHead className="p-2">Test</TableHead>
+                  <TableHead className="p-2">
+                    <span /*className="sr-only"*/>Actions</span>
+                  </TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {guests.map(guest => (
+                  <TableRow key={guest.id}>
+                    <TableCell className="font-medium p-2">{guest.guestID}</TableCell>
+                    <TableCell className="font-medium p-2">{guest.firstName} {guest.lastName}</TableCell>
+                    <TableHead className="font-medium p-2">Test</TableHead>
+                    <TableHead className="font-medium p-2">Test</TableHead>
+                    <TableHead className="font-medium p-2">Test</TableHead>
+                    <TableHead className="font-medium p-2">Test</TableHead>
+                    <TableHead className="font-medium p-2">Test</TableHead>
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button
+                            aria-haspopup="true"
+                            size="icon"
+                            variant="ghost"
+                          >
+                            <MoreHorizontal className="h-4 w-4" />
+                            <span className="sr-only">Toggle menu</span>
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                          {/* <DropdownMenuItem>Edit</DropdownMenuItem> */}
+                          <DropdownMenuItem onClick={() => handleDelete(guest.id)}>Delete</DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </TabsContent>
+      </Tabs>
       <CardFooter className='grid justify-end'>
         <Button >Send email to all guests</Button>
       </CardFooter>
