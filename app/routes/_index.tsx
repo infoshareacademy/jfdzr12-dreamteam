10a37371444a9ec7aa8c80e5b28acadef7cabe4d
Monika Pławska <monikaplawska88@gmail.com>
2024-04-23 11:56:30 +0200

    landing-page text

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 
 export const meta: MetaFunction = () => {
@@ -10,7 +11,23 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="grid place-items-center h-screen" /*style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}*/>
+    <>
+    <div className="grid place-items-center p-6 font-black" /*style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}*/>
+    Your perfect wedding starts here: DreamDay
     </div>
+    <div className="grid place-items-center">
+      <p>Plan your dream day. Create an event, guest list, budget.</p>
+      <p>Send your guests a link to a survey and get to know their preferences so you can organize your dream celebration together.</p>
+      <p>Manage your events.</p>
+    </div>
+    <div className="grid grid-cols-2 gap-4 justify-items-center p-6">
+      <div className="grid col-start-1 justify-items-center">
+        <Link to="sign-up">Young couple</Link>   
+      </div>
+      <div className="grid justify-items-center">
+        <Link to="guest">Guest</Link>
+      </div>
+    </div>
+    </>
   );
 }
