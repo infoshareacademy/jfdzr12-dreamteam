375a16a20fe8cd2d2add1189aa3495151f4529e9
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-04-24 23:06:16 +0200

    removed guest-list-form

--- app/routes/$currentUserUID.events.your-event.$eventID.guestlist.tsx
+++ app/routes/$currentUserUID.events.your-event.$eventID.guestlist.tsx
@@ -1,10 +1,8 @@
-import { GuestListForm } from "~/blocks/guest-list-form";
 import { GuestCard } from "~/pages/guest-card";
 
 export default function GuestListPage() {
   return (
     <div className="mx-screen mx-auto max-w-7xl">
-      <GuestListForm />
       <GuestCard />
     </div>
 
