e0590822722e8afc24fb503a31e54c4bc444e96b
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-04-24 23:04:45 +0200

    Added scroll area

--- app/blocks/guest-prefer-table.tsx
+++ app/blocks/guest-prefer-table.tsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from 'react';
 import { Table, TableBody, TableRow, TableCell, TableHead, TableHeader } from '~/atoms/ui/table';
 import { NewGuest } from '~/type/new-guest';
 import { guestRefOrder } from '~/db/guest-list-ref';
+import { ScrollBar } from '~/atoms/ui/scroll-area';
+import { ScrollArea } from '@radix-ui/react-scroll-area';
 
 const guestPrefHeader = [
+  'No.',
   'Partner',
   'Children',
-  'Menu',
-  'Partner menu',
-  'Child menu',
+  'Accommodation',
   'Transport',
   'Alcohol',
 ]
@@ -33,30 +34,32 @@ export const GuestPreferTable = () => {
   }, []);
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Name</TableHead>
-          {guestPrefHeader.map((header, index) => (
-            <TableHead key={index} className='text-center'>{header}</TableHead>
-          ))}
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {guests.map((guest) => (
-          <TableRow key={guest.id}>
-            <TableCell className="font-medium ">{guest.firstName} {guest.lastName}</TableCell>
-            {guest.formData ? <TableCell className="font-medium text-center font-normal">{guest.formData.partner}</TableCell> : <TableCell>{null}</TableCell>}
-            {guest.formData ? <TableCell className="font-medium text-center font-normal">{guest.formData.numberOfChildren}</TableCell> : <TableCell>{null}</TableCell>}
-            {guest.formData ? <TableCell className="font-medium text-center font-normal">{guest.formData.selectedMenuGuest}</TableCell> : <TableCell>{null}</TableCell>}
-            {guest.formData ? <TableCell className="font-medium text-center font-normal">{guest.formData.selectedMenuPartner}</TableCell> : <TableCell>{null}</TableCell>}
-            {guest.formData ? <TableCell className="font-medium text-center font-normal">{guest.formData.selectedMenuChild}</TableCell> : <TableCell>{null}</TableCell>}
-            {guest.formData ? <TableCell className="font-medium text-center font-normal">{guest.formData.transport}</TableCell> : <TableCell>{null}</TableCell>}
-            {guest.formData ? <TableCell className="font-medium text-center font-normal">{guest.formData.alcohols}</TableCell> : <TableCell>{null}</TableCell>}
+    <ScrollArea className="rounded-md border">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            {guestPrefHeader.map((header, index) => (
+              <TableHead key={index} className='text-center'>{header}</TableHead>
+            ))}
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {guests.map((guest, index) => (
+            <TableRow key={guest.id}>
+              <TableCell className="font-medium">{index + 1}</TableCell>
+              <TableCell className="font-medium ">{guest.firstName} {guest.lastName}</TableCell>
+              {guest.formData ? <TableCell className="font-medium text-center font-normal">{guest.formData.partner}</TableCell> : <TableCell>{null}</TableCell>}
+              {guest.formData ? <TableCell className="font-medium text-center font-normal">{guest.formData.numberOfChildren}</TableCell> : <TableCell>{null}</TableCell>}
+              {guest.formData ? <TableCell className="font-medium text-center font-normal">{guest.formData.accommodation}</TableCell> : <TableCell>{null}</TableCell>}
+              {guest.formData ? <TableCell className="font-medium text-center font-normal">{guest.formData.transport}</TableCell> : <TableCell>{null}</TableCell>}
+              {guest.formData ? <TableCell className="font-medium text-center font-normal max-w-36">{guest.formData.alcohols[0]} {guest.formData.alcohols[1]} {guest.formData.alcohols[2]} {guest.formData.alcohols[3]} {guest.formData.alcohols[4]} {guest.formData.alcohols[5]} {guest.formData.alcohols[6]}</TableCell> : <TableCell>{null}</TableCell>}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table >
+      <ScrollBar orientation='horizontal' />
+    </ScrollArea>
   )
 
 }
