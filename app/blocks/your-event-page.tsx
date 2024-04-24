b439139c02ba74a36a90d5797c23cfc005561717
Monika Pławska <monikaplawska88@gmail.com>
2024-04-24 17:19:14 +0200

    styles modified at your-event-page

--- app/blocks/your-event-page.tsx
+++ app/blocks/your-event-page.tsx
@@ -37,26 +37,10 @@ export default function YourEvent() {
         <>
             {eventData && (
                 <>
-                    {content && (
-                        <>
-                            <div className="flex items-center justify-center p-6">
-                                <h1 className="italic font-serif text-xl font-bold">{content}</h1>
-                            </div>
-                            <div className="flex items-center justify-center">
-                                <Heart/>
-                            </div>
-                            <div className="flex items-center justify-center p-6">
-                                <p>{eventDate}</p>
-                            </div>
-                            <div className="flex items-center justify-center">
-                                <Heart/>
-                            </div>
-                        </>
-                    )}
-                    <div className="flex items-center justify-center p-6">
-                        <p>{eventData.eventTime}</p>
+                    <div className="flex items-center justify-center mt-5 mb-10 p-6">
+                        <h1 className="italic font-serif text-xl font-bold">{content}</h1>
                     </div>
-                    <div className="grid grid-cols-2 gap-4 justify-items-center">
+                    <div className="grid grid-cols-3 gap-4 justify-items-center">
                         <div className="grid col-start-1 justify-items-center">
                             <HeartHandshake className="my-5"/>
                             <h1 className="mb-5">CEREMONY</h1>
@@ -64,6 +48,17 @@ export default function YourEvent() {
                             <p>{`Street: ${eventData.ceremonyStreetAddress}`}</p>
                             <p>{`City: ${eventData.ceremonyCityAddress}`}</p>
                             <p>{`Country: ${eventData.ceremonyCountryAddress}`}</p>
+                        </div>
+                        <div className="grid justify-items-center">
+                            <div className="flex items-center justify-center p-6">
+                                <p>{eventDate}</p>
+                            </div>
+                            <div className="flex items-center justify-center">
+                                <Heart/>
+                            </div>
+                            <div className="flex items-center justify-center p-6">
+                                <p>{eventData.eventTime}</p>
+                            </div>
                         </div>
                         <div className="grid justify-items-center">
                             <Wine className="my-5"/>
@@ -74,15 +69,17 @@ export default function YourEvent() {
                             <p>{`Country: ${eventData.receptionCountryAddress}`}</p>
                         </div>
                     </div>
-                    <div className="flex h-[100px] items-center justify-center p-6">
-                        <p>{`Event code: ${eventData.eventID}`}</p>
-                    </div>
                     {eventData.other && (
-                        <div className="flex items-center justify-center p-6 mb-10">
-                            <p>{`Other: ${eventData.other}`}</p>
+                        <>
+                        <div className="flex items-center justify-center mt-20">
+                            <p>OTHER</p>
                         </div>
+                        <div className="flex items-center justify-center p-6 whitespace-pre-wrap">
+                            <p>{eventData.other}</p>
+                        </div>
+                        </>
                     )}
-                    <div className="grid grid-cols-4 gap-4 justify-items-center m-10 sticky bottom-0">
+                    <div className="grid grid-cols-4 gap-4 justify-items-center py-20 sticky top-100">
                         <div className="col-start-1">
                             <Link to="guestlist">Guest list</Link>
                         </div>
