c376e44caaddd21488415ddd67908acc0e14b24c
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-04-26 23:44:44 +0200

    landing page done

--- app/blocks/landing-page.tsx
+++ app/blocks/landing-page.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@remix-run/react";
+import { Button } from "~/atoms/ui/button";
 import { useCurrentUser } from "~/db/auth";
 
 export default function LandingPage() {
@@ -6,26 +7,23 @@ export default function LandingPage() {
     const unauthenticated = user.status === "unauthenticated";
 
     return (
-        <div className="grid h-screen">
-            <div className="grid place-items-center text-center" /*style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}*/>
-                <p>Your perfect wedding starts here...</p>
-                <h1 className="font-bold text-2xl">DreamDay</h1>
-            </div>
-            <div className="grid place-items-center text-center">
-                <p>Plan your dream day. Create an event, guest list, budget.</p>
-                <p>Send your guests a link to a survey and get to know their preferences so you can organize your dream celebration together.</p>
-                <p>Manage your events.</p>
-            </div>
-            {unauthenticated &&
-            <div className="grid grid-cols-2 gap-4 justify-items-center p-6">
-                <div className="grid col-start-1 justify-items-center">
-                    <Link to="sign-up" className="h-10 w-full px-4 py-2 bg-transparent border border-gray-300 text-secondary-foreground hover:bg-secondary/80 hover:border-transparent inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">Young couple</Link>   
+        <div className="h-screen bg-hero-pattern bg-cover bg-bottom fixed top-0 left-0 right-0">
+            <div className="grid gap-4 md:gap-6 lg:gap-6 absolute top-16 md:top-20 lg:top-24 inset-x-10 md:inset-x-20 lg:inset-x-56 h-3/5 lg:h-auto" /*style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}*/>
+                <div className="grid justify-center content-end text-center">
+                    <h1 className="scroll-m-20 text-3xl font-bold md:text-5xl lg:text-5xl">Your perfect &thinsp;wedding <br />&ensp;starts here... DreamDay</h1>
                 </div>
-                <div className="grid justify-items-center">
-                    <Link to="guest" className="h-10 w-full px-4 py-2 bg-transparent border border-gray-300 text-secondary-foreground hover:bg-secondary/80 hover:border-transparent inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50y">Guest</Link>
+                <div className="grid place-items-center text-center">
+                    <h3 className="text-sm md:text-lg lg:text-lg">Plan your dream day. Create an event, guest list, budget.
+                        Send your guests a link to a survey and get to know their preferences so you can organize your dream celebration together. Manage your events.</h3>
                 </div>
+                {unauthenticated &&
+                    <div className="flex justify-around flex-wrap flex-auto gap-x-6 gap-y-2 lg:pt-6">
+                        <Button size='lg' className="w-40 justify-self-start"><Link to="sign-up">Young couple</Link></Button>
+                        <Button variant='mainOutline' size='lg' className="w-40 justify-self-end"><Link to="guest">Guest</Link></Button>
+                    </div>
+
+                }
             </div>
-            }
         </div>
     );
 }
