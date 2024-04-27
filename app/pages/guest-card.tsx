89b5939493958f025eda3368839f7eeb27e25526
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-04-27 10:52:42 +0200

    styled guest list &related guest list

--- app/pages/guest-card.tsx
+++ app/pages/guest-card.tsx
@@ -8,9 +8,9 @@ import { GuestListForm } from "~/blocks/guest-list-form";
 export const GuestCard = () => {
 
   return (<>
-    <div className="fixed z-10 h-screen bg-table-pattern bg-cover bg-bottom top-0 left-0 right-0">
-    </div>
-    <Card className="absolute z-20 top-20 inset-x-1/2 -translate-x-1/2 w-80 sm:w-11/12 md:w-11/12 lg:w-10/12 xl:w-10/12 2xl:w-9/12">
+    {/* <div className="fixed z-10 h-screen bg-table-pattern bg-cover bg-bottom top-0 left-0 right-0">
+    </div> */}
+    <Card className="absolute z-20 top-20 inset-x-1/2 -translate-x-1/2 w-80 sm:w-11/12 lg:w-10/12 2xl:w-9/12">
       <CardHeader className="grid auto-cols-auto sm:grid-cols-3 gap-4 justify-center">
         <div className="col-start-1 col-end-3">
           <CardTitle>Your Guests</CardTitle>
--- app/pages/guest-related-e-card.tsx
+++ app/pages/guest-related-e-card.tsx
@@ -5,9 +5,9 @@ import { RelatedEventGuestListTable } from "~/blocks/related-e-guest-list-table"
 export const RelatedEventGuestCard = () => {
 
   return (<>
-    <div className="fixed z-10 h-screen bg-table-pattern bg-cover bg-bottom top-0 left-0 right-0">
-    </div>
-    <Card className="absolute z-20 top-20 inset-x-1/2 -translate-x-1/2 w-80 sm:w-11/12 md:w-11/12 lg:w-10/12 xl:w-10/12 2xl:w-9/12">
+    {/* <div className="fixed z-10 h-screen bg-table-pattern bg-cover bg-bottom top-0 left-0 right-0">
+    </div> */}
+    <Card className="absolute z-20 top-20 inset-x-1/2 -translate-x-1/2 w-80 sm:w-11/12 lg:w-10/12 2xl:w-9/12">
       <CardHeader className="grid auto-cols-auto sm:grid-cols-3 gap-4 justify-center">
         <div className="col-start-1 col-end-3">
           <CardTitle>Your Guests</CardTitle>
