dafc511eb007c1c63746e8f586b58439d476ac79
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-04-27 22:57:12 +0200

    styling register and log in

--- app/routes/sign-up.tsx
+++ app/routes/sign-up.tsx
@@ -1,6 +1,12 @@
-import {SignUp} from "~/blocks/sign-up"
-export default function SignUpPage (){
-return (
-<SignUp/>
-)}
+import { SignUp } from "~/blocks/sign-up"
 
+export default function SignUpPage() {
+  return (
+    <div className="w-full h-screen grid lg:grid-cols-2 justify-center content-center">
+      <div>
+        <SignUp />
+      </div>
+      <div className="lg:w-1/2 lg:h-screen lg:bg-sign-up-pattern lg:bg-cover lg:bg-bottom lg:fixed lg:top-0 lg:left-1/2 lg:right-0" />
+    </div>
+  )
+}
